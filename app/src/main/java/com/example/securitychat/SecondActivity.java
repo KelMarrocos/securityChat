ec0913ad85ee6f026ab05b558e9157716d14362f
kelMarrocos <keltonopm@gmail.com>
2024-07-19 21:31:04 -0300

    Start Commit!

--- app/src/main/java/com/example/securitychat/SecondActivity.java
+++ app/src/main/java/com/example/securitychat/SecondActivity.java
@@ -1,9 +1,6 @@
 package com.example.securitychat;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class homeScreen extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_screen);
-
+        setContentView(R.layout.activity_second);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button buttonOpenLogin = findViewById(R.id.button);
-        buttonOpenLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(homeScreen.this, LoginActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
