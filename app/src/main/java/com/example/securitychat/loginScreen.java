b48da2dcb3134d241b855173c7c94771b59339a6
kelMarrocos <keltonopm@gmail.com>
2024-07-20 15:58:17 -0300

    Start Commit!

--- app/src/main/java/com/example/securitychat/loginScreen.java
+++ app/src/main/java/com/example/securitychat/loginScreen.java
@@ -20,7 +20,7 @@ public class loginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_login_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
